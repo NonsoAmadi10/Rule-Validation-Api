9f17d84d3bd449afb0b1b7b85618e0be34008bc3
NonsoAmadi10 <nonsoamadi@aol.com>
2021-01-29 22:40:09 +0100

    (main): deploy to heroku

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { config } from 'dotenv';
+import cors from "cors";
 import routes from './routes';
 
 
@@ -7,14 +8,20 @@ import routes from './routes';
 
 config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use('/', routes);
-
+app.set('x-powered-by', false);
 
 
 
 const port = process.env.PORT || 3000 ;
+
+app.all('*', (req, res) => res.status(404).json({
+    success: false,
+    message: 'The page you are looking for does not exist'
+  }));
 
 app.listen(port, () => console.log("Server Running..."));
 
